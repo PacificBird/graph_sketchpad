bef20968d4baa5f0da90102f226d517a1eefbf2b
Izzy @PacificBird <izzymacster@gmail.com>
2023-12-10 23:50:59 -0800

    Edges and deletion now fully functional!

--- src/app.rs
+++ src/app.rs
@@ -4,16 +4,21 @@ use egui_plot::{Line, Plot, Points};
 pub enum Selection {
     Vertex,
     Edge,
+    Delete,
 }
 
 pub struct TemplateApp {
     selected: Selection,
+    graph: petgraph::graph::UnGraph<Vertex, Edge>,
+    drag: Option<petgraph::graph::NodeIndex>,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             selected: Selection::Vertex,
+            graph: petgraph::graph::Graph::new_undirected(),
+            drag: None,
         }
     }
 }
@@ -26,7 +31,6 @@ impl TemplateApp {
 }
 
 impl eframe::App for TemplateApp {
-    /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
@@ -46,36 +50,185 @@ impl eframe::App for TemplateApp {
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.selectable_value(&mut self.selected, Selection::Vertex, "V");
-            ui.selectable_value(&mut self.selected, Selection::Edge, "E");
-        });
+        let panel_response = egui::SidePanel::left("side_panel")
+            .show(ctx, |ui| {
+                ui.selectable_value(&mut self.selected, Selection::Vertex, "V");
+                ui.selectable_value(&mut self.selected, Selection::Edge, "E");
+                ui.selectable_value(&mut self.selected, Selection::Delete, "D");
+            })
+            .response;
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let plt = Plot::new("plot");
-            let plt = plt.allow_zoom([false, false]);
-            let plt = plt.show_grid([false, false]);
-            let plt = plt.show_axes([false, false]);
-            let plt = plt.allow_scroll(false);
-            let plt = plt.allow_drag(false);
-            let plt = plt.show_x(false);
-            let plt = plt.show_y(false);
-            plt.show(ui, |plot_ui| {})
-        });
+        let plot_response = egui::CentralPanel::default()
+            .show(ctx, |ui| {
+                let plt = Plot::new("plot");
+                let plt = plt.allow_zoom([false, false]);
+                let plt = plt.show_grid([false, false]);
+                let plt = plt.show_axes([false, false]);
+                let plt = plt.allow_scroll(false);
+                let plt = plt.allow_drag(false);
+                let plt = plt.allow_boxed_zoom(false);
+                plt.show(ui, |plot_ui| {
+                    for node_idx in self.graph.node_indices() {
+                        plot_ui.points({
+                            let node = self.graph.node_weight(node_idx).expect("No node weight");
+                            node.to_points(plot_ui.transform())
+                                .color(node.color)
+                                .radius(5.0)
+                        })
+                    }
+                    for edge_idx in self.graph.edge_indices() {
+                        let (i1, i2) = self
+                            .graph
+                            .edge_endpoints(edge_idx)
+                            .expect("There must be an edge at this index");
+                        let pos1 = self
+                            .graph
+                            .node_weight(i1)
+                            .expect("There must be a node at this index")
+                            .to_point(plot_ui.transform());
+                        let point1 = egui_plot::PlotPoint::new(pos1.x, pos1.y);
+                        let pos2 = self
+                            .graph
+                            .node_weight(i2)
+                            .expect("There must be a node at this index")
+                            .to_point(plot_ui.transform());
+                        let point2 = egui_plot::PlotPoint::new(pos2.x, pos2.y);
+                        plot_ui.line(
+                            egui_plot::Line::new(egui_plot::PlotPoints::Owned(vec![
+                                point1, point2,
+                            ]))
+                            .stroke(egui::Stroke::new(
+                                2.0,
+                                self.graph
+                                    .edge_weight(edge_idx)
+                                    .expect("There must be an edge at this index")
+                                    .color,
+                            )),
+                        );
+                    }
+                })
+            })
+            .response;
+        let plot_response = plot_response.interact(egui::Sense::click().union(egui::Sense::drag()));
+
+        match self.selected {
+            Selection::Vertex => {
+                if let Some(pos) = plot_response.interact_pointer_pos() {
+                    if plot_response.clicked() {
+                        println!("clicked!");
+                        let idx = get_close_node(pos, &self.graph);
+                        if self.drag.is_some() {
+                            self.drag = None;
+                        } else if let Some(i) = idx {
+                            self.drag = Some(petgraph::graph::NodeIndex::new(i));
+                        } else {
+                            self.graph
+                                .add_node(Vertex::new(pos, egui::Color32::from_white_alpha(255)));
+                            println!("{}", self.graph.node_count());
+                        }
+                    }
+                }
+                if let (Some(i), Some(pos)) = (self.drag, plot_response.hover_pos()) {
+                    self.graph
+                        .node_weight_mut(i)
+                        .expect("No node weight")
+                        .move_to(pos);
+                }
+            }
+            Selection::Edge => {
+                if let Some(pos) = plot_response.interact_pointer_pos() {
+                    println!("{:?}, {}", self.drag, self.graph.edge_count());
+                    if plot_response.clicked() {
+                        let idx = get_close_node(pos, &self.graph);
+                        if let Some(i) = self.drag {
+                            if let Some(i2) = idx {
+                                self.graph.add_edge(
+                                    i,
+                                    petgraph::graph::NodeIndex::new(i2),
+                                    Edge::new(egui::Color32::from_white_alpha(255)),
+                                );
+                            }
+                            self.drag = None;
+                        } else if let Some(i) = idx {
+                            println!("new drag");
+                            self.drag = Some(petgraph::graph::NodeIndex::new(i));
+                        }
+                    }
+                }
+            }
+            Selection::Delete => {
+                if let Some(pos) = plot_response.interact_pointer_pos() {
+                    let idx = get_close_node(pos, &self.graph);
+                    if plot_response.clicked() {
+                        if let Some(i) = self.drag {
+                            println!("no new drag");
+                            if let Some(i2) = idx {
+                                if let Some(e) =
+                                    self.graph.find_edge(i, petgraph::graph::NodeIndex::new(i2))
+                                {
+                                    self.graph.remove_edge(e);
+                                }
+                            }
+                            self.drag = None;
+                        } else if let Some(i) = idx {
+                            println!("new drag");
+                            self.drag = Some(petgraph::graph::NodeIndex::new(i));
+                        }
+                    }
+                    if plot_response.secondary_clicked() {
+                        if let Some(i) = idx {
+                            self.graph.remove_node(petgraph::graph::NodeIndex::new(i));
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 struct Vertex {
-    x: f64,
-    y: f64,
-    color: String,
+    pos: egui::Pos2,
+    color: egui::Color32,
+}
+impl Vertex {
+    fn new(pos: egui::Pos2, color: egui::Color32) -> Self {
+        Self { pos, color }
+    }
+    fn to_points(self, transform: &egui_plot::PlotTransform) -> egui_plot::Points {
+        egui_plot::Points::new(egui_plot::PlotPoints::Owned(vec![
+            transform.value_from_position(self.pos)
+        ]))
+    }
+    fn to_point(self, transform: &egui_plot::PlotTransform) -> egui_plot::PlotPoint {
+        transform.value_from_position(self.pos)
+    }
+    // fn translate(&mut self, delta: egui::Vec2) {
+    //     (*self).pos = self.pos + egui::Vec2::new(10.0, 10.0);
+    // }
+    fn move_to(&mut self, pos: egui::Pos2) {
+        (*self).pos = pos;
+    }
 }
 struct Edge {
-    color: String,
+    color: egui::Color32,
+}
+impl Edge {
+    fn new(color: egui::Color32) -> Self {
+        Self { color }
+    }
 }
 
-struct GraphPlot {
-    graph: petgraph::graph::UnGraph<Vertex, Edge>,
+fn get_close_node(
+    pos: egui::Pos2,
+    graph: &petgraph::graph::UnGraph<Vertex, Edge>,
+) -> Option<usize> {
+    graph.node_indices().position(|x| {
+        graph
+            .node_weight(x)
+            .expect("No node weight")
+            .pos
+            .distance(pos)
+            < 15.0
+    })
 }
-
-impl GraphPlot {}
